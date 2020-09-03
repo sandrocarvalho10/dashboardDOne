b0dc86d07a26cb48f42cc31a8a32b9c627dd95dd
Sandro Carvalho <sandrinho9797@outlook.com>
2020-09-03 11:46:30 -0300

    Adiciona Variavel Ambiente para variavel API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL : 'https://api-d1-test.herokuapp.com/api/'});
+const api = axios.create({ baseURL : process.env.REACT_APP_API_URL});
 
 
 
